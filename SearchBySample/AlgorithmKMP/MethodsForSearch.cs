ef0d427e80a4d386d9b325dc532f53f2db7e02e7
Nikita <nikita.ushakov.02@outlook.com>
2023-04-01 15:57:56 +0200

    dev: start preparation to develop search by sample

---
+++ SearchBySample/AlgorithmKMP/MethodsForSearch.cs
@@ -0,0 +1,52 @@
+﻿
+
+using System.Text;
+
+namespace AlgorithmKMP
+{
+    /// <summary>
+    /// Класс с методами для чтения данных и поиска по образцу
+    /// </summary>
+    /// <typeparam name="T"> Тип данных, который будет у значения узла </typeparam>
+    public class MethodsForSearch<T>
+    {
+
+
+        public MethodsForSearch(Dictionary<T, List<T>> nodes)
+        {
+            
+        }
+
+        /// <summary>
+        /// Получение данных из файла
+        /// </summary>
+        /// <param name="data"> Передаваемые данные файла </param>
+        /// <returns></returns>
+        static public Dictionary<char, List<char>> CreateGraphFromMatrixAdjacency(FileStream data)
+        {
+            // выделяем массив для считывания данных из файла
+            byte[] buffer = new byte[matrixAdjacency.Length];
+            // считываем данные
+            matrixAdjacency.Read(buffer, 0, buffer.Length);
+            // декодируем байты в строку (получаем строку)
+            string matrixFromFile = Encoding.Default.GetString(buffer);
+
+            string[] rowsMatrix = matrixFromFile.Split(new string[] { "\n" }, StringSplitOptions.None);
+
+            // Работаем с каждым узлом графа в отдельности
+            for (int i = 0; i < rowsMatrix.Length; i++)
+            {
+                var valuesLine = rowsMatrix[i].Split(new char[] { ' ' });
+
+                // Создаём новый узел и добавляем в него связанные узлы
+                allNodes.Add(GetNeedSymbol(i), new List<char>() { });
+
+                for (int j = 0; j < valuesLine.Length; j++)
+                    if (Convert.ToInt32(valuesLine[j]) == 1)
+                        allNodes[GetNeedSymbol(i)].Add(GetNeedSymbol(j));
+            }
+
+            return allNodes;
+        }
+    }
+}
---
+++ SearchBySample/AlgorithmKMP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AlgorithmKMP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Берём файл для чтения матрицы
+            string path = @"D:\dream\Algorithms\Graphs\SearchBySample\data.txt";
+            var fileMatrixIncidence = new FileStream(path, FileMode.OpenOrCreate);
+
+
+        }
+    }
+}
