db890dffdbf0730eaf7faea3babe6d5c77de4d3a
Nikita <nikita.ushakov.02@outlook.com>
2023-04-02 10:33:08 +0200

    dev: change structure develop search

---
+++ SearchBySample/SearchBySample/MethodsForSearch.cs
@@ -0,0 +1,28 @@
+﻿namespace AlgorithmKMP
+{
+    /// <summary>
+    /// Класс с методами для чтения данных и поиска по образцу
+    /// </summary>
+    public class MethodsForSearch
+    {
+        public string Text;
+
+        public MethodsForSearch(string text)
+        {
+            Text = text;
+        }
+
+        /// <summary>
+        /// Реализация Алгоритма Кнута-Морриса-Пратта
+        /// </summary>
+        /// <param name="sample"> Передаваемая строка образец </param>
+        /// <returns></returns>
+        public string AlgorithmKMP(string sample)
+        {
+            string textFromFile = "";
+            
+
+            return textFromFile;
+        }
+    }
+}
---
+++ SearchBySample/SearchBySample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace AlgorithmKMP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Берём файл для чтения матрицы
+            string path = @"D:\dream\Algorithms\SearchBySample\AlgorithmKMP\data.txt";
+            var fileWithText = new StreamReader(path);
+
+            string? text = fileWithText.ReadLine();
+            fileWithText?.Close();
+
+            Console.WriteLine("Enter word sample: ");
+            string sample = Console.ReadLine();
+
+            var methods = new MethodsForSearch(text);
+            methods.AlgorithmKMP(sample);
+        }
+    }
+}
+// Исходная строка считывается из некоторого файла. Строка поиска вводится с клавиатуры.
+
+// Реализовать алгоритм Кнута-Морриса-Пратта для поиска по образцу
