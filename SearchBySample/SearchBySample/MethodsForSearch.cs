7937bbe014754d339ddaf26f4b2aaffcadc95442
Nikita <nikita.ushakov.02@outlook.com>
2023-04-23 21:22:20 +0200

    add: Algorithm Boyer-Moore and Algorithm KMP

--- SearchBySample/SearchBySample/MethodsForSearch.cs
+++ SearchBySample/SearchBySample/MethodsForSearch.cs
@@ -25,7 +25,7 @@ namespace AlgorithmKMP
         /// Простой поиск образца в строке «методом грубой силы»
         /// </summary>
         /// <param name="sample"> Передаваемая строка образец </param>
-        public void SimpleSearch(string sample)
+        public void SimpleSearch(string pattern)
         {
             // i-с какого места строки  ищем
             // j-с какого места образца ищем
@@ -33,10 +33,10 @@ namespace AlgorithmKMP
             {
                 for (int j = 0; ; ++j)
                 {
-                    if (Text[i + j] != sample[j])
+                    if (Text[i + j] != pattern[j])
                         break;
 
-                    if ((j + 1) == sample.Length)
+                    if ((j + 1) == pattern.Length)
                         Console.Write($"Substring is finded. It starts with {(i + 1)} elements!");
                 }
             }
@@ -47,238 +47,77 @@ namespace AlgorithmKMP
         /// <summary>
         /// Реализация Алгоритма Кнута-Морриса-Пратта
         /// </summary>
-        /// <param name="sample"> Передаваемая строка образец </param>
-        /// <returns></returns>
-        public void AlgorithmKMP(string sample)
+        public void AlgorithmKMP(string pattern)
         {
-            var lengthText = Text.Length;
-            var lengthSample = sample.Length;
+            int M = pattern.Length;
+            int N = Text.Length;
 
-            // Массив длин префиксов для образца (\сколько символов в образце)
-            var pi = new int[lengthSample];
-            // p[0]=0 всегда, p[1]=1, если начинается с двух одинаковых 
-            pi[0] = 0;
+            // create lps[] that will hold the longest prefix suffix values for pattern
+            int[] lps = new int[M];
+            int j = 0; // index for pat[]
 
-            // длина образца
-            int l;
+            // Preprocess the pattern (calculate lps[] array)
+            ComputeLPSArray(pattern, M, lps);
 
-            // Заполняем массив для префиксов
-            for (l = 1; l < lengthSample; ++l)
+            int i = 0; // index for txt[]
+            while ((N - i) >= (M - j))
             {
-                int k = pi[l - 1];
-
-                // Если совпадение, то берем ранее рассчитанное значение (начиная с максимально возможных)
-                while ((k > 0) && (sample[l] != sample[k]))
-                    k = pi[k - 1];
-
-                if (sample[l] == sample[k])
-                    ++k;
-
-                pi[l] = k;
-            }
-
-            // количество совпавших символов, оно же индекс сравниваемого 
-            // символа в образце. В строке сравниваемый символ будет иметь индекс i
-            int j = 0;
-
-            for (int i = 1; i < lengthText; ++i)
-            {
-                // Символ строки не совпал с символом в образце. Сдвигаем образец, зная, что первые
-                // j символов образца совпали с символами строки и надо сравнить j+1й символ образца
-                // (его индекс j) с i+1м символом строки. если j=0, то достигли начала образца
-                while ((j > 0) && (Text[i] != sample[j]))
-                    j = pi[j - 1];
-
-                // есть совпадение очередного символа увеличиваем длину совпавшего фрагмента
-                if (Text[i] == sample[j])
-                    ++j;
-
-                // Когда образец найден
-                // Вычисляется и выводится индекс начала найденного в str образца 
-                if (j == l)
+                if (pattern[j] == Text[i])
                 {
-                    Console.WriteLine($"With this index start word in text: {(i - l + 1)}");
-
-                    for (int k = 0; k < lengthSample; ++k)
-                        Console.Write(Text[(i - l + 1) + k]);
-
-                    // Можно не возвращать, а искать дальше совпадения
-                    return;
+                    j++;
+                    i++;
                 }
-            }
-        }
 
-        /// <summary>
-        /// Реализация префикс функции
-        /// </summary>
-        public List<int> PrefixFunction()
-        {
-            int lengthText = Text.Length;
-            var pi = new List<int>(lengthText); // в i-м элементе (его индекс i-1) количество совпавших символов в начале и конце для подстроки длины i. 
-                                                // p[0]=0 всегда, p[1]=1, если начинается с двух одинаковых 
-            for (int i = 1; i < lengthText; ++i)
-            {
-                // ищем, какой префикс-суффикс можно расширить
-                int j = pi[i - 1]; // длина предыдущего префикса-суффикса, возможно нулевая
-                while ((j > 0) && (Text[i] != Text[j])) // этот нельзя расширить,
-                    j = pi[j - 1];   // берем длину меньшего префикса-суффикса
-
-                if (Text[i] == Text[j])
-                    ++j;  // расширяем найденный (возможно пустой) префикс-суффикс
-                pi[i] = j;
-            }
-            return pi;
-        }
-
-        /// <summary>
-        /// Реализация Алгоритма Бойера Мура (недоработанная)
-        /// </summary>
-        /// <param name="sample"> Передаваемая строка образец </param>
-        /// <returns></returns>
-        public void AlgorithmBoyerMoore_2(string sample)
-        {
-            // Создаём таблицу смещений и суффиксов
-            var offset = CreateOffsetTable(sample);
-            var suffix = CreateSuffixTable(sample);
-            var suffix2 = CreateSuffixTable_2(sample);
-
-            //всегда либо bound = 0, либо bound = m - suffshift[0]
-            int j, bound = 0;
-
-            int m = sample.Length;
-            int n = Text.Length;
-
-            for (int i = 0; i <= n - m; i += suffix[j + 1])
-                for (j = m - 1; j >= bound && sample[j] == Text[i + j]; j--)
+                if (j == M)
                 {
-                    if (j < bound)
-                    {
-                        Console.Write($"Substring is finded. It starts with {i} elements!");
-                        bound = m - suffix[0];
-                        j = -1; //установить j так, как будто мы прочитали весь шаблон s, а не только до границы bound
-                    }
+                    Console.WriteLine("Found pattern at index " + (i - j));
+                    j = lps[j - 1];
+                }
+
+                // mismatch after j matches
+                else if (i < N && pattern[j] != Text[i])
+                {
+                    // Do not match lps[0..lps[j-1]] characters, they will match anyway
+                    if (j != 0)
+                        j = lps[j - 1];
                     else
-                        bound = 0;
-                }
-
-            Console.Write($"Substring wasn't finded.");
-
-            /*int t = 0;
-            int last = sample.Length - 1;
-
-            while (t < Text.Length - last)
-            {
-                int p = last;
-
-                while (p >= 0 && Text[t + p] == sample[p])
-                    p--;
-
-                if (p == -1)
-                {
-                    Console.Write($"Substring is finded. It starts with {t} elements!");
-                    return;
-                }
-
-                // Возвращает большее из двух чисел
-                t += Math.Max(p - offset[Text[t + p]], suffix[p + 1]);
-            }
-
-            Console.Write($"Substring wasn't finded.");*/
-        }
-
-        /// <summary>
-        /// Построение таблицы смещений
-        /// </summary>
-        public Dictionary<char, int> CreateOffsetTable(string pattern)
-        {
-            // количество символов зависит от алфавита с которым мы работаем
-            var offset = new Dictionary<char, int>();
-
-            // Заполняем таблицу смещений
-            // Заполняем каждого символа его последнее упоминание в образце (элементы считаются с позиции 1)
-            for (int i = 0; i < pattern.Length - 1; i++)
-            {
-                if (!offset.TryAdd(pattern[i], i + 1))
-                    offset[pattern[i]] = i + 1;
-            }
-
-            return offset;
-        }
-
-        /// <summary>
-        /// Построение таблицы суффиксов
-        /// </summary>
-        public List<int> CreateSuffixTable(string pattern)
-        {
-            int m = pattern.Length;
-            List<int> suffshift = new List<int>(m + 1);
-
-            for (int i = 0; i < m + 1; i++)
-                suffshift.Add(m);
-
-            List<int> z = new List<int>(m);
-
-            for (int i = 0; i < m; i++)
-                z.Add(0);
-
-            for (int j = 1, maxZidx = 0, maxZ = 0; j < m; ++j)
-            {
-                if (j <= maxZ)
-                    z[j] = Math.Min(maxZ - j + 1, z[j - maxZidx]);
-
-                while (j + z[j] < m && pattern[m - 1 - z[j]] == pattern[m - 1 - (j + z[j])])
-                    z[j]++;
-
-                if (j + z[j] - 1 > maxZ)
-                {
-                    maxZidx = j;
-                    maxZ = j + z[j] - 1;
+                        i = i + 1;
                 }
             }
-
-            // Первый цикл
-            for (int j = m - 1; j > 0; j--)
-                suffshift[m - z[j]] = j;
-
-            // Второй цикл
-            for (int j = 1, r = 0; j <= m - 1; j++)
-                if (j + z[j] == m)
-                    for (; r <= j; r++)
-                        if (suffshift[r] == m) suffshift[r] = j;
-
-            return suffshift;
         }
 
-        /// <summary>
-        /// Построение таблицы суффиксов
-        /// </summary>
-        public List<int> CreateSuffixTable_2(string pattern)
+        public void ComputeLPSArray(string pat, int M, int[] lps)
         {
-            var suffix = new List<int>(pattern.Length + 1);
+            // length of the previous longest prefix suffix
+            int len = 0;
+            int i = 1;
+            lps[0] = 0; // lps[0] is always 0
 
-            for (int i = 0; i < pattern.Length + 1; i++)
-                suffix.Add(pattern.Length);
-
-            suffix[pattern.Length] = 1;
-
-            for (int i = pattern.Length - 1; i >= 0; i--)
-                for (int at = i; at < pattern.Length; at++)
+            // the loop calculates lps[i] for i = 1 to M-1
+            while (i < M)
+            {
+                if (pat[i] == pat[len])
                 {
-                    string s = pattern.Substring(at);
-
-                    for (int j = at - 1; j >= 0; j--)
+                    len++;
+                    lps[i] = len;
+                    i++;
+                }
+                else // (pat[i] != pat[len])
+                {
+                    // This is tricky. Consider the example. AAACAAAA and i = 7. The idea is similar to search step.
+                    if (len != 0)
                     {
-                        string p = pattern.Substring(j, s.Length);
-                        if (p == s)
-                        {
-                            suffix[i] = at - 1;
-                            at = pattern.Length;
-                            break;
-                        }
+                        len = lps[len - 1];
+
+                        // Also, note that we do not increment i here
+                    }
+                    else // if (len == 0)
+                    {
+                        lps[i] = len;
+                        i++;
                     }
                 }
-
-            return suffix;
+            }
         }
 
         /// <summary>
@@ -286,99 +125,59 @@ namespace AlgorithmKMP
         /// </summary>
         public void AlgorithmBoyerMoore(string pattern)
         {
-            // s is shift of the pattern 
-            // with respect to text
-            int s = 0, j;
+            /* A pattern searching function that uses Bad Character Heuristic of Boyer Moore Algorithm */
+
             int m = pattern.Length;
             int n = Text.Length;
 
-            int[] bpos = new int[m + 1];
-            int[] shift = new int[m + 1];
+            int[] badchar = new int[allNumberSymbols];
 
-            // initialize all occurrence of shift to 0
-            for (int i = 0; i < m + 1; i++)
-                shift[i] = 0;
+            /* Fill the bad character array by calling the preprocessing function badCharHeuristic() for given pattern */
+            BadCharHeuristic(pattern, m, badchar);
 
-            // do preprocessing
-            preprocess_strong_suffix(shift, bpos, pattern.ToCharArray(), m);
-            preprocess_case2(shift, bpos, pattern.ToCharArray(), m);
-
-            while (s <= n - m)
+            // s is shift of the pattern with respect to text
+            int s = 0; 
+            
+            while (s <= (n - m))
             {
-                j = m - 1;
+                int j = m - 1;
 
-                /* Keep reducing index j of pattern while 
-                characters of pattern and text are matching 
-                at this shift s*/
+                /* Keep reducing index j of pattern while characters of pattern and text are matching at this shift s */
                 while (j >= 0 && pattern[j] == Text[s + j])
                     j--;
 
-                /* If the pattern is present at the current shift, 
-                then index j will become -1 after the above loop */
+                /* If the pattern is present at current shift, then index j will become -1 after the above loop */
                 if (j < 0)
                 {
-                    Console.Write("pattern occurs at shift = {0}\n", s);
-                    s += shift[0];
+                    Console.WriteLine("Patterns occur at shift = " + s);
+
+                    /* Shift the pattern so that the next character in text aligns with the last occurrence of it in pattern.
+                    The condition s+m < n is necessary for the case when pattern occurs at the end of text */
+                    s += (s + m < n) ? m - badchar[Text[s + m]] : 1;
                 }
+
                 else
-
-                    /*pat[i] != pat[s+j] so shift the pattern
-                    shift[j+1] times */
-                    s += shift[j + 1];
+                    /* Shift the pattern so that the bad character in text aligns with the last occurrence of it in pattern. The max function is used to
+                    make sure that we get a positive shift. We may get a negative shift if the last occurrence of bad character in pattern
+                    is on the right side of the current character. */
+                    s += Max(1, j - badchar[Text[s + j]]);
             }
         }
 
-        // preprocessing for strong good suffix rule
-        private void preprocess_strong_suffix(int[] shift, int[] bpos, char[] pat, int m)
+        private int Max(int a, int b) { return (a > b) ? a : b; }
+
+        private void BadCharHeuristic(string str, int size, int[] badchar)
         {
-            // m is the length of pattern 
-            int i = m, j = m + 1;
-            bpos[i] = j;
+            int i;
 
-            while (i > 0)
-            {
-                /*if character at position i-1 is not 
-                equivalent to character at j-1, then 
-                continue searching to right of the
-                pattern for border */
-                while (j <= m && pat[i - 1] != pat[j - 1])
-                {
-                    /* the character preceding the occurrence of t 
-                    in pattern P is different than the mismatching 
-                    character in P, we stop skipping the occurrences 
-                    and shift the pattern from i to j */
-                    if (shift[j] == 0)
-                        shift[j] = j - i;
+            // Initialize all occurrences as -1
+            for (i = 0; i < allNumberSymbols; i++)
+                badchar[i] = -1;
 
-                    //Update the position of next border 
-                    j = bpos[j];
-                }
-                /* p[i-1] matched with p[j-1], border is found.
-                store the beginning position of border */
-                i--; j--;
-                bpos[i] = j;
-            }
-        }
-
-        //Preprocessing for case 2
-        private void preprocess_case2(int[] shift, int[] bpos, char[] pat, int m)
-        {
-            int i, j;
-            j = bpos[0];
-            for (i = 0; i <= m; i++)
-            {
-                /* set the border position of the first character 
-                of the pattern to all indices in array shift
-                having shift[i] = 0 */
-                if (shift[i] == 0)
-                    shift[i] = j;
-
-                /* suffix becomes shorter than bpos[0], 
-                use the position of next widest border
-                as value of j */
-                if (i == j)
-                    j = bpos[j];
-            }
+            // Fill the actual value of last occurrence
+            // of a character
+            for (i = 0; i < size; i++)
+                badchar[(int)str[i]] = i;
         }
 
         /// <summary>
--- SearchBySample/SearchBySample/Program.cs
+++ SearchBySample/SearchBySample/Program.cs
@@ -5,22 +5,22 @@
         static void Main(string[] args)
         {
             // Берём файл для чтения матрицы
-            string path = @"D:\dream\Algorithms\SearchBySample\SearchBySample\data2.txt";
+            string path = @"D:\dream\Algorithms\SearchBySample\SearchBySample\data1.txt";
             //string path = @"D:\workSpaceNU\primat\Algorithms\SearchBySample\SearchBySample\data.txt";
             var fileWithText = new StreamReader(path);
             var text = fileWithText.ReadToEnd();
             fileWithText?.Close();
 
             //string sample = "игла";
-            //string sample = "ERG";
-            string sample = "AABA";
+            string sample = "ERG";
+            //string sample = "AABA";
 
             var methods = new MethodsForSearch(text);
 
             //methods.SimpleSearch(sample);
             //methods.AlgorithmKMP(sample);
-            //methods.AlgorithmBoyerMoore(sample);
-            methods.AlgorithmRabinCarp(sample);
+            methods.AlgorithmBoyerMoore(sample);
+            //methods.AlgorithmRabinCarp(sample);
             //methods.FiniteStateMachine(sample);
         }
     }
