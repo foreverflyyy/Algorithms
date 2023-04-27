80eaf1d4304e7d3f107d07888a2f9c09cb0f70d3
Nikita <nikita.ushakov.02@outlook.com>
2023-04-27 08:15:16 +0200

    add: solution about task sum subset

---
+++ NP-completeness/NP-tasks/MethodsForDecisions.cs
@@ -0,0 +1,71 @@
+﻿namespace NpTasks
+{
+    /// <summary>
+    /// Класс с методами для решения NP-задач
+    /// </summary>
+    public static class MethodsForDecisions
+    {
+        /// <summary>
+        /// Решение задачи о рюкзаке с помощью динамического программирования
+        /// </summary>
+        /// <param name="thingsWithValues"></param>
+        /// <param name="maxWeight"> Вместимость рюкзака </param>
+        public static void TaskAboutBackpack(List<Thing> thingsWithValues, int maxWeight)
+        {
+            
+        }
+        
+        /// <summary>
+        /// Решение задачи о раскраске графа
+        /// </summary>
+        public static void TaskAboutColoringGraph()
+        {
+            
+        }/// <summary>
+        /// Решение задачи о раскладке по ящикам
+        /// </summary>
+        public static void TaskLayoutByBoxes()
+        {
+            
+        }/// <summary>
+        /// Решение задачи о суммах подмножеств, используя жадный алгоритм
+        /// </summary>
+        public static void isSubsetSum(HashSet<string> allComponents, Dictionary<string, HashSet<string>> tableSubsetsStations)
+        {
+            // Хранение итогового набора станций
+            var finalStations= new List<string>();
+
+            while (allComponents.Any())
+            {
+                // Поиск станции, которая обслуживает больше всего штантов, не входящих в текущее покрытие
+                string bestStation = "";
+
+                // Содержит все штаты, обслуживаемые этой станцией, которые ещё не входят в текущее покрытие
+                List<string> statesCovered = new List<string>();
+
+                // Перебор всех станций и поиск наилучшей
+                foreach (var subsets in tableSubsetsStations)
+                {
+                    var covered = allComponents.Intersect(subsets.Value); // Пересечение множеств
+
+                    if (covered.Count() > statesCovered.Count())
+                    {
+                        bestStation = subsets.Key;
+                        statesCovered = covered.ToList();
+                    }
+                }
+
+                // Удаляем добаленные станции из общего списка и добавляем множество в итоговый список
+                foreach(var state in statesCovered)
+                    allComponents.Remove(state);
+
+                finalStations.Add(bestStation);
+            }
+
+            Console.WriteLine("List with components: ");
+
+            foreach (var component in finalStations)
+                Console.Write($"{component}, ");
+        }
+    }
+}
---
+++ NP-completeness/NP-tasks/Program.cs
@@ -0,0 +1,78 @@
+﻿
+
+namespace NpTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача о рюкзаке
+            TaskAboutBackpack();
+
+
+            // Задача о раскраске графа
+            //TaskAboutColoringGraph();
+
+
+            // Задача о распределении в ящики
+            //TaskLayoutByBoxes();
+
+
+            // Задача о сумме подмножеств
+            //isSubsetSum();
+        }
+
+        public static void TaskAboutBackpack()
+        {
+            var thingsWithValues = new Dictionary<string, HashSet<string>>();
+
+            thingsWithValues.Add("kone", new HashSet<string> { "id", "nv", "ut" });
+            thingsWithValues.Add("ktwo", new HashSet<string> { "wa", "id", "mt" });
+            thingsWithValues.Add("kthree", new HashSet<string> { "or", "nv", "ca" });
+            thingsWithValues.Add("kfour", new HashSet<string> { "nv", "ut" });
+            thingsWithValues.Add("kfive", new HashSet<string> { "ca", "az" });
+
+            MethodsForDecisions.TaskAboutBackpack();
+        }
+
+        public static void TaskAboutColoringGraph()
+        {
+            MethodsForDecisions.TaskAboutColoringGraph();
+        }
+
+        public static void TaskLayoutByBoxes()
+        {
+            MethodsForDecisions.TaskLayoutByBoxes();
+        }
+        
+        public static void isSubsetSum()
+        {
+            var allComponents = new HashSet<string>()
+            {
+                "mt", "or", "wa", "id", "nv", "ut", "ca", "az"
+            };
+            var tableSubsetsStations = new Dictionary<string, HashSet<string>>();
+
+            tableSubsetsStations.Add("kone", new HashSet<string> { "id", "nv", "ut" });
+            tableSubsetsStations.Add("ktwo", new HashSet<string> { "wa", "id", "mt" });
+            tableSubsetsStations.Add("kthree", new HashSet<string> { "or", "nv", "ca" });
+            tableSubsetsStations.Add("kfour", new HashSet<string> { "nv", "ut" });
+            tableSubsetsStations.Add("kfive", new HashSet<string> { "ca", "az" });
+
+            MethodsForDecisions.isSubsetSum(allComponents, tableSubsetsStations);
+        }
+    }
+
+    public class Thing
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }
+}
+/*
+  NP - полные задачи
+    Решить задачу о раскраске графа.
+    Решить дискретную задачу о рюкзаке.
+    Решить задачу о раскладке по ящикам
+    Решить задачу о суммах подмножеств, используя жадный алгоритм.
+*/
