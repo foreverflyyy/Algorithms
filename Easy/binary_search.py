69f5a5b525e0b1f5a9c8f2e2727e12b90ccb4c26
Nikita <nikita.ushackow.02@gmail.com>
2024-01-30 11:36:40 +0200

    added some solutions

--- Easy/binary_search.py
+++ Easy/binary_search.py
@@ -1,5 +1,11 @@
 import math
-from typing import List
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
@@ -16,10 +22,88 @@ class Solution:
 
         return -1
 
+    def isSymmetric(self, root: Optional[TreeNode], first=None, second=None) -> bool:
+        if root:
+            return self.isSymmetric(None, root.left, root.right)
+
+        if not first and not second:
+            return True
+        if not first or not second:
+            return False
+        if first.val != second.val:
+            return False
+
+        check_1 = self.isSymmetric(None, first.left, second.right)
+        check_2 = self.isSymmetric(None, first.right, second.left)
+        return check_1 and check_2
+
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        in_indexes = {}
+        for i, num in enumerate(inorder):
+            in_indexes[num] = i
+
+        def helper(low, high):
+            if low > high:
+                return None
+
+            root = TreeNode(postorder.pop())
+            mid = in_indexes[root.val]
+            root.right = helper(mid + 1, high)
+            root.left = helper(low, mid - 1)
+            return root
+
+        return helper(0, len(postorder) - 1)
+
+    # def buildTree(self, inorder, postorder):
+    #     if not inorder or not postorder:
+    #         return None
+    #
+    #     root = TreeNode(postorder.pop())
+    #     in_order_index = inorder.index(root.val)
+    #
+    #     root.right = self.buildTree(inorder[in_order_index + 1:], postorder)
+    #     root.left = self.buildTree(inorder[:in_order_index], postorder)
+    #     return root
+
+    def build_tree_by_pre_and_in_order(self, pre_order: List[int], in_order: List[int]) -> Optional[TreeNode]:
+        def helper(pre_start, in_start, in_end, pre_order, in_order):
+            if pre_start > len(pre_order) - 1 or in_start > in_end or in_start == -1:
+                return None
+
+            root = TreeNode(pre_order[pre_start])
+            in_index = -2
+            for i in range(in_start, (in_end + 1)):
+                if in_order[i] == root.val:
+                    in_index = i
+                    break
+
+            root.left = helper(
+                pre_start + 1,
+                in_start,
+                in_index - 1,
+                pre_order,
+                in_order
+            )
+            root.right = helper(
+                pre_start + in_index - in_start + 1,
+                in_index + 1,
+                in_end,
+                pre_order,
+                in_order
+            )
+            return root
+        return helper(0, 0, len(in_order) - 1, pre_order, in_order)
+
 
 solution = Solution()
-print(solution.search([-1,0,3,5,9,12], 9))
-print(solution.search([-1,0,3,5,9,12], 2))
-print(solution.search([-1,0,3,5,9,12], -2))
-print(solution.search([-1,0,3,5,9,12], 12))
-print(solution.search([5], 5))
+# print(solution.search([-1,0,3,5,9,12], 9))
+# print(solution.search([-1,0,3,5,9,12], 2))
+
+# tr3 = TreeNode(2)
+# tr2 = TreeNode(2)
+# tr1 = TreeNode(1, tr2, tr3)
+# print(solution.isSymmetric(tr1))
+
+# print(solution.buildTree([3,9,20,15,7], [9,3,15,20,7]))
+res = solution.buildTree([9,3,15,20,7], [9,15,7,20,3])
+print(res)
